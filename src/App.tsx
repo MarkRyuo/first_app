8fa973592065c8c5b577e70c0f9c77b909ec9ca8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-10 10:23:28 +0800

    update here

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 // import Message from './Message'
 import ListGroup from "./components/ListGroup";
 
-function App() {
-  return (
-    <div>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
-    </div>
-  );
+function App(){
+  return <div><ListGroup/></div>
 }
 
-export default App;
+export default App ;
