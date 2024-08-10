3685e4d9f88b173e225de12d261718a44d1958f3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-10 14:38:10 +0800

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,2 +1,17 @@
+import { Fragment } from "react/jsx-runtime"
 
 
+function NavBar() {
+    return <Fragment>
+    <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top">
+            Bootstrap
+            </a>
+        </div>
+    </nav>
+    </Fragment>
+}
+
+export default NavBar ;
