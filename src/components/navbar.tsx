feaa70535ca2a66b01d0125aa8baf79a24ba3a22
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-10 15:17:32 +0800

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ function NavBar() {
             Navbar
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
@@ -17,10 +17,10 @@ function NavBar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
                   Home
