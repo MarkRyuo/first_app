5b4a7018619035a51f8aed20b4c0c285a282f7e7
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-10 10:29:45 +0800

    update here

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,7 @@ import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
     return (
     <Fragment>
+        <h1>This is a class</h1>
         <ul className="list-group">
             <li className="list-group-item">An item</li>
             <li className="list-group-item">A second item</li>
