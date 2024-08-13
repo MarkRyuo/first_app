357d0b42cb67a069145360e63c6e1f79b7453e21
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-13 13:56:29 +0800

    Update Aug 13 2024

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -10,15 +10,19 @@ function ListGroup() {
 
 
     return (
-    <Fragment> 
-        { 
-        // Fragment for use another element, to comment use curly braces
-        } 
-        <h1>This is a List group</h1>
-        <ul className="list-group">
-            {items.map(item => <li>{item}</li>)}
-        </ul>
-    </Fragment>
+      <Fragment>
+        {
+          // Fragment for use another element, to comment use curly braces
+        }
+        <div>
+          <h1>This is a List group</h1>
+          <ul className="list-group">
+            {items.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </Fragment>
     );
 }
 
