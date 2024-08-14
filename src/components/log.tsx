97517881d03a5dae2069a68515261e8a81b68aec
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-14 19:39:53 +0800

    Update Aug 14 2024

--- src/components/log.tsx
+++ src/components/log.tsx
@@ -8,9 +8,9 @@ function Log() {
                 <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
 
-            <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text" id="basic-addon2">@example.com</span>
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                <span className="input-group-text" id="basic-addon2">@example.com</span>
             </div>
 
             <div className="mb-3">
