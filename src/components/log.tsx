4e3df1846c10097b68fcf0b28307a98fe18c4f6c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-14 19:37:16 +0800

    Update Aug 14 2024

--- src/components/log.tsx
+++ src/components/log.tsx
@@ -17,9 +17,9 @@ function Log() {
             <label for="basic-url" class="form-label">Your vanity URL</label>
             <div class="input-group">
                 <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
+                <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4"/>
             </div>
-            <div class="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
+            <div className="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
             </div>
 
             <div className="input-group mb-3">
