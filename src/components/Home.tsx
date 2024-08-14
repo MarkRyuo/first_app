0bb14ee0b896158e74fb7d4adc193a1b6b629f18
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-14 15:19:04 +0800

    Update Aug 14 2024

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,14 @@
 
 
 function HomeScreen() {
+    const NAMES = [
+        "Jhon Mark"
+    ]
+
     return (
-        <>
+        <> {
+            // This is Comment 
+            }
             <div className="container-md">
                 <nav className="container-sm">
                     <button type="button" className="btn btn-success">Watch Video</button>
