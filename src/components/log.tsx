58f51423558cb029283748f74f9c8bce11f75379
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-14 19:31:33 +0800

    Update Aug 14 2024

--- src/components/log.tsx
+++ src/components/log.tsx
@@ -2,10 +2,11 @@
 
 function Log() {
     return (
-        <div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-</div>
+        <>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">@</span>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
 
 <div class="input-group mb-3">
   <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
@@ -37,6 +38,7 @@ function Log() {
   <span class="input-group-text">With textarea</span>
   <textarea class="form-control" aria-label="With textarea"></textarea>
 </div>
+        </>
     ) ;
 }
 
