f66b1bf5180650c52df2901ce3ba7630ef91d0c6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-14 15:33:04 +0800

    Update Aug 14 2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Message from './components/Message'
 import HomeScreen from "./components/Home";
 import ListGroup from "./components/ListGroup";
-// import NavBar from "./components/navbar";
+import NavBar from "./components/navbar";
 
 function App(){
   return <div>
+    <NavBar/>
     <HomeScreen/>
     <Message/>
     <ListGroup/>
