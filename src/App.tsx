2d48af09f4d074e662edd6dd16cca172b26023a6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-17 18:22:36 +0800

    Update: Aug 17 2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Message from './components/Message'
+// import Message from './components/Message'
 import HomeScreen from "./components/Home";
-import ListGroup from "./components/ListGroup";
-import NavBar from "./components/navbar";
-import Log from './components/log';
+// import ListGroup from "./components/ListGroup";
+// import NavBar from "./components/navbar";
+// import Log from './components/log';
 
 function App(){
   return <div>
