4e528f5c440b67f21d7acfbf247145b10e318ea2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-17 17:35:13 +0800

    Update: Aug 17 2024

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,32 +1,32 @@
 function HomeScreen() {
-  let Names = ["Jhon Mark", "Leomar", "Neil", "Laud"];
+    let Names = ["Jhon Mark", "Leomar", "Neil", "Laud"];
 
-  Names = [];
+    Names = [];
 
   // if(Names.length == 0 ) {
   //     return <p>No Value found</p> ;
   // }
 
-  return (
-    <>
-        {
-        // * This is Comment
-        }
-            <div className="container-lg">
-                <nav className="container-sm">
-                    <button type="button" className="btn btn-success">
-                        Watch Video
-                    </button>
-                    <p className="H1">List</p>
-                    {/* 
-                        condition ? < expression if true> : < expression if false>
-                    */}
-                    {Names.length == 0 ? <p>No Value found</p> : null}
+    return (
+        <>
+            {
+            // * This is Comment
+            }
+                <div className="container-lg">
+                    <nav className="container-sm">
+                        <button type="button" className="btn btn-success">
+                            Watch Video
+                        </button>
+                        <p className="H1">List</p>
+                        {/* 
+                            condition ? < expression if true> : < expression if false>
+                        */}
+                        {Names.length == 0 ? <p>No Value found</p> : null}
 
-                    {Names.map((names) => (<p className="bg-warning">{names}</p>))}
-                </nav>
-            </div>
-        </>
-    );
+                        {Names.map((names) => (<p className="bg-warning">{names}</p>))}
+                    </nav>
+                </div>
+            </>
+        );
 }
 export default HomeScreen;
